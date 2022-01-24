ed91ff113f1e529c43dd81d1123a18f57baaf744
Alexander <alez.grante@gmail.com>
2022-01-24 19:53:09 +0300

    description xml added

---
+++ app/src/main/java/com/example/starwarscharacters/presentation/ui/description/DescriptionViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.starwarscharacters.presentation.ui.description
+
+import androidx.lifecycle.ViewModel
+
+class DescriptionViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
