1c53283026ca292f96543e13a48f5df507ca7419
Alexander <alez.grante@gmail.com>
2022-01-25 01:27:39 +0300

    RepImpl

--- app/src/main/java/com/example/starwarscharacters/data/repository/CharactersRepositoryImpl.kt
+++ app/src/main/java/com/example/starwarscharacters/data/repository/CharactersRepositoryImpl.kt
@@ -2,15 +2,21 @@ package com.example.starwarscharacters.data.repository
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import com.example.starwarscharacters.data.database.AppDatabase
+import com.example.starwarscharacters.data.database.CharacterDao
 import com.example.starwarscharacters.domain.Character
 import com.example.starwarscharacters.domain.CharactersRepository
 
 class CharactersRepositoryImpl(application: Application) : CharactersRepository {
+
+    private val characterDao = AppDatabase.getInstance(application).characterDao()
+
+
     override fun getCharacter(name: String): LiveData<Character> {
-        TODO("Not yet implemented")
+        characterDao.getCharacter(name)
     }
 
     override fun getCharacterList(): LiveData<List<Character>> {
-        TODO("Not yet implemented")
+        characterDao.getCharacterList()
     }
 }
