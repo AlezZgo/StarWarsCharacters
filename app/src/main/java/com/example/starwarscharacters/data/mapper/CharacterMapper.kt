8e3c50df3f0100ee7b86c227e45b11396f747d90
Alexander <alez.grante@gmail.com>
2022-01-25 22:43:45 +0300

    mapper Dto to DbModel

--- app/src/main/java/com/example/starwarscharacters/data/mapper/CharacterMapper.kt
+++ app/src/main/java/com/example/starwarscharacters/data/mapper/CharacterMapper.kt
@@ -1,15 +1,25 @@
 package com.example.starwarscharacters.data.mapper
 
 import com.example.starwarscharacters.data.database.CharacterInfoDbModel
+import com.example.starwarscharacters.data.network.model.CharacterDto
 import com.example.starwarscharacters.domain.CharacterInfo
 
 class CharacterMapper {
     fun mapDbModelToEntity(infoDbModel: CharacterInfoDbModel) = CharacterInfo(
-        infoDbModel.name,
-        infoDbModel.gender,
-        infoDbModel.mass,
-        infoDbModel.height,
-        infoDbModel.homeWorld,
-        infoDbModel.films
+        name = infoDbModel.name,
+        gender = infoDbModel.gender,
+        mass = infoDbModel.mass,
+        height = infoDbModel.height,
+        homeWorld = infoDbModel.homeWorld,
+        films = infoDbModel.films
+    )
+
+    fun mapDtoToDbModel(characterDto: CharacterDto) = CharacterInfoDbModel(
+        name = characterDto.name,
+        gender = characterDto.gender,
+        mass = characterDto.mass.toString(),
+        height = characterDto.height.toString(),
+        homeWorld = characterDto.homeworld,
+        films = characterDto.films.joinToString(separator = ",")
     )
 }
