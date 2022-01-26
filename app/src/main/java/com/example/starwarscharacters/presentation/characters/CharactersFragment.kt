99d1878829da57ed78902a8c84335f9c04776667
Alexander <alez.grante@gmail.com>
2022-01-26 17:17:14 +0300

    RecycleView Works

--- app/src/main/java/com/example/starwarscharacters/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/starwarscharacters/presentation/characters/CharactersFragment.kt
@@ -1,19 +1,29 @@
 package com.example.starwarscharacters.presentation.characters
 
 import android.os.Bundle
+import android.os.Debug
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.starwarscharacters.data.database.AppDatabase
+import com.example.starwarscharacters.data.database.CharacterInfoDbModel
 import com.example.starwarscharacters.databinding.FragmentCharactersBinding
 import com.example.starwarscharacters.domain.entities.CharacterInfo
 import com.example.starwarscharacters.presentation.adapter.CharactersAdapter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class CharactersFragment : Fragment() {
 
-    private lateinit var charactersViewModel: CharactersViewModel
+    private val viewModel: CharactersViewModel by lazy {
+        ViewModelProvider(this).get(CharactersViewModel::class.java)
+    }
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding
@@ -25,29 +35,22 @@ class CharactersFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCharactersBinding.inflate(inflater, container, false)
-        return binding.root
-    }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        charactersViewModel =
-            ViewModelProvider(this)[CharactersViewModel::class.java]
-
-        setAdapter()
-
-    }
-
-    fun setAdapter(){
         val adapter = CharactersAdapter(requireContext())
+
         adapter.onCharacterClickListener = object : CharactersAdapter.OnCharacterClickListener{
             override fun onCharacterClick(characterInfo: CharacterInfo) {
                 Toast.makeText(requireContext(),"hello",Toast.LENGTH_LONG).show()
             }
         }
         binding.rvCharacters.adapter = adapter
-        charactersViewModel.characterList.observe(requireActivity()){
+
+        viewModel.characterList.observe(viewLifecycleOwner){
             adapter.submitList(it)
         }
+
+        return binding.root
     }
+
+
 }
