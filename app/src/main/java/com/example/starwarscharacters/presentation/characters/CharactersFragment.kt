0bed5060d1838d58340a4816f87f58f436b1f2c0
Alexander <alez.grante@gmail.com>
2022-01-30 04:25:57 +0300

    refactored with BaseFragment

--- app/src/main/java/com/example/starwarscharacters/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/starwarscharacters/presentation/characters/CharactersFragment.kt
@@ -1,49 +1,37 @@
 package com.example.starwarscharacters.presentation.characters
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
-import android.widget.Toast
-import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.example.starwarscharacters.data.database.AppDatabase
-import com.example.starwarscharacters.data.database.CharacterDao
-import com.example.starwarscharacters.data.database.CharacterInfoDbModel
-import com.example.starwarscharacters.data.datasource.RemoteDataSourceImpl
-import com.example.starwarscharacters.data.mapper.CharacterMapper
 import com.example.starwarscharacters.databinding.FragmentCharactersBinding
 import com.example.starwarscharacters.domain.entities.CharacterInfo
+import com.example.starwarscharacters.presentation.BaseFragment
 import com.example.starwarscharacters.presentation.adapter.CharactersAdapter
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 
-class CharactersFragment : Fragment() {
+class CharactersFragment : BaseFragment<FragmentCharactersBinding>() {
 
     private val viewModel: CharactersViewModel by lazy {
         ViewModelProvider(this).get(CharactersViewModel::class.java)
     }
 
-    private var _binding: FragmentCharactersBinding? = null
-    private val binding: FragmentCharactersBinding
-        get() = _binding ?: throw RuntimeException("FragmentCharactersBinding is null ")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    private val remoteDataSourceImpl = RemoteDataSourceImpl()
-    private lateinit var characterDao : CharacterDao
-    private val mapper = CharacterMapper()
+        setAdapter()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View {
-        _binding = FragmentCharactersBinding.inflate(inflater, container, false)
+        setFilter()
 
+    }
+
+    private fun setAdapter(){
         val adapter = CharactersAdapter(requireContext())
 
         adapter.onCharacterClickListener = object : CharactersAdapter.OnCharacterClickListener {
@@ -51,9 +39,9 @@ class CharactersFragment : Fragment() {
                 launchDescriptionFragment(character)
             }
         }
-        adapter.onIsFavouriteClickListener = object : CharactersAdapter.OnIsFavouriteClickListener{
+        adapter.onIsFavouriteClickListener = object : CharactersAdapter.OnIsFavouriteClickListener {
             override fun onIsFavouriteClick(character: CharacterInfo) {
-                CoroutineScope(Dispatchers.IO + Job() ).launch{
+                CoroutineScope(Dispatchers.IO + Job()).launch {
                     viewModel.insertCharacterUseCase(character.copy(isFavourite = !character.isFavourite))
                 }
             }
@@ -64,10 +52,10 @@ class CharactersFragment : Fragment() {
         viewModel.characterList.observe(viewLifecycleOwner) {
             adapter.submitList(it)
         }
+    }
 
-
-
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+    private fun setFilter(){
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let { viewModel.setFilter(it) }
                 return false
@@ -79,13 +67,17 @@ class CharactersFragment : Fragment() {
                 return false
             }
         })
-
-        return binding.root
     }
 
-    fun launchDescriptionFragment(character: CharacterInfo){
+
+
+    private fun launchDescriptionFragment(character: CharacterInfo) {
         findNavController().navigate(
             CharactersFragmentDirections.actionNavigationCharactersToDescriptionFragment(character)
         )
     }
+
+    override fun initBinding(
+        inflater: LayoutInflater, container: ViewGroup?,
+    ): FragmentCharactersBinding = FragmentCharactersBinding.inflate(inflater, container, false)
 }
